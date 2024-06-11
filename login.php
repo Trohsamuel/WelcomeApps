c4628c802ebb9286bdb209a6431a0cf75ae43a3c
Tedyan27 <tedyan27@gmail.com>
2024-06-11 16:21:20 +0100

    Initial commit

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $file = fopen("users.txt", "r");
+    $validUser = false;
+
+    while (($line = fgets($file)) !== false) {
+        list($storedUser, $storedPass) = explode(",", trim($line));
+        if ($username == $storedUser && $password == $storedPass) {
+            $validUser = true;
+            break;
+        }
+    }
+    fclose($file);
+
+    if ($validUser) {
+        $_SESSION['username'] = $username;
+        header("Location: welcome.php");
+        exit();
+    } else {
+        echo "Invalid credentials.";
+    }
+}
+?>
---
+++ welcome.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: index.html");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
+    <p>You have successfully logged in.</p>
+</body>
+</html>
